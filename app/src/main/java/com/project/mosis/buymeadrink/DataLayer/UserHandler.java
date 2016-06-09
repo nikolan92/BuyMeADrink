545e6e8d31340a8a8f45294ae7d66b9ccf4c328f
nikolan92 <92nikolan@gmail.com>
2016-06-09 21:40:55 +0200

    Added Volley

--- app/src/main/java/com/project/mosis/buymeadrink/DataLayer/UserHandler.java
+++ app/src/main/java/com/project/mosis/buymeadrink/DataLayer/UserHandler.java
@@ -9,35 +9,39 @@ public class UserHandler {
     public UserHandler(User user){
         this.user = user;
     }
-    public boolean LogIn(){
+    public boolean LogIn(String Tag){
 
         //TODO: Add code for log in (Requset to server with Volley)
         return true;
     }
-    public boolean LogOut(){
+    public boolean LogOut(String Tag){
 
         //TODO: Add code for log out (Requset to server with Volley)
         return true;
     }
-    public String Register(){
+    public String Register(String Tag){
 
         //TODO: Add code for Register (Requset to server with Volley)
         return "Some response from server";
     }
-    public boolean UpdateInfo(){
+    public boolean UpdateInfo(String Tag){
         //TODO: Add code for UpdateInfo (Requset to server with Volley)
         return true;
     }
-    public boolean ReiseRank(){
+    public boolean ReiseRank(String Tag){
         //TODO: Add code for RaiseRank (Requset to server with Volley)
         return true;
     }
-    public boolean getFriends(){
+    public boolean GetFriends(String Tag){
         //TODO: Add code for getFriends (Requset to server with Volley)
         return true;
     }
 
-    public User getUser(){
+    public User GetUser(){
         return this.user;
+    }
+
+    public void CancelAllRequest(String Tag){
+        //TODO: When activity call onStop() this function must be caled because if you not call this fun Volley will call your handler and app will crash
     }
 }
