85684ab0ca6bfd297f75d31a3cd9b2946f76cd2e
nikolan92 <92nikolan@gmail.com>
2016-06-17 20:43:56 +0200

    Some tests in UserProfileActivity

--- app/src/main/java/com/project/mosis/buymeadrink/UserProfileActivity.java
+++ app/src/main/java/com/project/mosis/buymeadrink/UserProfileActivity.java
@@ -1,13 +1,58 @@
 package com.project.mosis.buymeadrink;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class UserProfileActivity extends AppCompatActivity {
+    final int PICK_IMAGE_REQUEST = 1;
+    private ImageView imageView;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        imageView = (ImageView)findViewById(R.id.user_profile_imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+                Toast.makeText(UserProfileActivity.this,"Ss",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Setting the Bitmap to ImageView
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
