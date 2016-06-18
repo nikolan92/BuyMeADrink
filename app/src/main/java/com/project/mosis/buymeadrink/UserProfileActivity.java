4083364ff8e01681a8d6ceeedbc94731a30e40fe
nikolan92 <92nikolan@gmail.com>
2016-06-18 13:44:43 +0200

    Removed some unused code.

--- app/src/main/java/com/project/mosis/buymeadrink/UserProfileActivity.java
+++ app/src/main/java/com/project/mosis/buymeadrink/UserProfileActivity.java
@@ -21,7 +21,6 @@ import com.theartofdev.edmodo.cropper.CropImageView;
 import java.io.IOException;
 
 public class UserProfileActivity extends AppCompatActivity {
-    final int PICK_IMAGE_REQUEST = 1;
     private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,36 +33,17 @@ public class UserProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 CropImage.startPickImageActivity(UserProfileActivity.this);
-                //Toast.makeText(UserProfileActivity.this,"Ss",Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void showFileChooser() {
-//        //TODO:Make dialog with two choices "Camera" or "Gallery" and start diferent intent
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        galleryIntent.setType("image/*");
-//        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-//        startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_IMAGE_REQUEST);
-        startActivityForResult(galleryIntent,PICK_IMAGE_REQUEST);
-
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-//            Uri selectedImageUri = data.getData();
-//            //Toast.makeText(this,selectedImageUri.getPath(),Toast.LENGTH_LONG).show();
-//
-//            CropImage.activity(selectedImageUri)
-//                    .setGuidelines(CropImageView.Guidelines.ON)
-//                    .setFixAspectRatio(true)
-//                    .start(this);
-//        }
         if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             Uri selectedImageUri = CropImage.getPickImageResultUri(this, data);
-            
+
             CropImage.activity(selectedImageUri)
                     .setGuidelines(CropImageView.Guidelines.ON)
                     .setFixAspectRatio(true)
