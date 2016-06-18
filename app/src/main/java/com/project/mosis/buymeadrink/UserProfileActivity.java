6f057023c4b62210596d9b605f33ff5012dd939e
nikolan92 <92nikolan@gmail.com>
2016-06-18 13:36:40 +0200

    Some tests in UserProfileActivity

--- app/src/main/java/com/project/mosis/buymeadrink/UserProfileActivity.java
+++ app/src/main/java/com/project/mosis/buymeadrink/UserProfileActivity.java
@@ -1,57 +1,83 @@
 package com.project.mosis.buymeadrink;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageOptions;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.IOException;
 
 public class UserProfileActivity extends AppCompatActivity {
     final int PICK_IMAGE_REQUEST = 1;
     private ImageView imageView;
-    private Bitmap bitmap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        imageView = (ImageView)findViewById(R.id.user_profile_imageView);
+        imageView = (ImageView) findViewById(R.id.user_profile_imageView);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showFileChooser();
-                Toast.makeText(UserProfileActivity.this,"Ss",Toast.LENGTH_SHORT).show();
+                CropImage.startPickImageActivity(UserProfileActivity.this);
+                //Toast.makeText(UserProfileActivity.this,"Ss",Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void showFileChooser() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+//        //TODO:Make dialog with two choices "Camera" or "Gallery" and start diferent intent
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryIntent.setType("image/*");
+//        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+//        startActivityForResult(Intent.createChooser(galleryIntent, "Select Picture"), PICK_IMAGE_REQUEST);
+        startActivityForResult(galleryIntent,PICK_IMAGE_REQUEST);
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri filePath = data.getData();
-            try {
-                //Getting the Bitmap from Gallery
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                //Setting the Bitmap to ImageView
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
+//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+//            Uri selectedImageUri = data.getData();
+//            //Toast.makeText(this,selectedImageUri.getPath(),Toast.LENGTH_LONG).show();
+//
+//            CropImage.activity(selectedImageUri)
+//                    .setGuidelines(CropImageView.Guidelines.ON)
+//                    .setFixAspectRatio(true)
+//                    .start(this);
+//        }
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri selectedImageUri = CropImage.getPickImageResultUri(this, data);
+            
+            CropImage.activity(selectedImageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setFixAspectRatio(true)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                imageView.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Log.e("UserProfileActivity",error.toString());
             }
         }
     }
