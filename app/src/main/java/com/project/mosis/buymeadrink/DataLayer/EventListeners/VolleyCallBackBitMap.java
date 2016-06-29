5c387ba62c63251820b63662e806baafc6ae63f3
nikolan92 <92nikolan@gmail.com>
2016-06-29 16:34:50 +0200

    VolleyCallBack problem

--- app/src/main/java/com/project/mosis/buymeadrink/DataLayer/EventListeners/VolleyCallBackBitMap.java
+++ app/src/main/java/com/project/mosis/buymeadrink/DataLayer/EventListeners/VolleyCallBackBitMap.java
@@ -2,8 +2,10 @@ package com.project.mosis.buymeadrink.DataLayer.EventListeners;
 
 import android.graphics.Bitmap;
 
+
 public interface VolleyCallBackBitmap {
 
+    //git problem
     void onSuccess(Bitmap result);
 
     void onFailed(String error);
